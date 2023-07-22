8e10399def7207d2db55290ed44cf6a9ed4e00df
André Skjelbred <skjelbred321@gmail.com>
2023-07-22 06:01:40 +0200

    ji

--- src/components/generalGameComponents/sidebar/sidebar.component.js
+++ src/components/generalGameComponents/sidebar/sidebar.component.js
@@ -3,6 +3,7 @@ import { GiSoccerField } from "react-icons/gi";
 import { FaRoute } from "react-icons/fa";
 import { BsArrowDownUp } from "react-icons/bs";
 import { FaQuestion } from "react-icons/fa";
+import Link from "next/link";
 
 function Sidebar({ guessedPlayers }) {
   return (
@@ -10,36 +11,36 @@ function Sidebar({ guessedPlayers }) {
       <div className='sidebar'>
         <ul className='sidebar-nav'>
           <li className='sidebar-nav-item'>
-            <a href='#' className='sidebar-link'>
+            <Link href='/guess-eleven' className='sidebar-link'>
               <span className='sidebar-icon'>
                 <GiSoccerField />
               </span>
               <span className='sidebar-text'>Guess 11</span>
-            </a>
+            </Link>
           </li>
           <li className='sidebar-nav-item'>
-            <a href='#' className='sidebar-link'>
+            <Link href='/career-path' className='sidebar-link'>
               <span className='sidebar-icon'>
                 <FaRoute />
               </span>
               <span className='sidebar-text'>Career Path</span>
-            </a>
+            </Link>
           </li>
           <li className='sidebar-nav-item'>
-            <a href='#' className='sidebar-link'>
+            <Link href='/hidden-player' className='sidebar-link'>
               <span className='sidebar-icon'>
                 <FaQuestion />
               </span>
               <span className='sidebar-text'>Blurred Player</span>
-            </a>
+            </Link>
           </li>
           <li className='sidebar-nav-item'>
-            <a href='#' className='sidebar-link'>
+            <Link href='/higher-lower' className='sidebar-link'>
               <span className='sidebar-icon'>
                 <BsArrowDownUp />
               </span>
               <span className='sidebar-text'>Higher or Lower</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
