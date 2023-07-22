d35cc4fbaf8d232304a1b7ee798aa0c27c54bb95
André Skjelbred <skjelbred321@gmail.com>
2023-07-22 05:54:28 +0200

    ijjdw2

--- src/components/generalGameComponents/sidebar/sidebar.component.js
+++ src/components/generalGameComponents/sidebar/sidebar.component.js
@@ -1,33 +1,34 @@
+import Link from "next/link";
 import { useSelector } from "react-redux";
 
 function Sidebar({ guessedPlayers }) {
   return (
     <div>
-      <div className="sidebar">
-        <ul className="sidebar-nav">
-          <li className="sidebar-nav-item">
-            <a href="#" className="sidebar-link">
-              <span className="sidebar-icon">1</span>
-              <span className="sidebar-text">Guess 11</span>
-            </a>
+      <div className='sidebar'>
+        <ul className='sidebar-nav'>
+          <li className='sidebar-nav-item'>
+            <Link href='/guess-eleven' className='sidebar-link'>
+              <span className='sidebar-icon'>1</span>
+              <span className='sidebar-text'>Guess 11</span>
+            </Link>
           </li>
-          <li className="sidebar-nav-item">
-            <a href="#" className="sidebar-link">
-              <span className="sidebar-icon">2</span>
-              <span className="sidebar-text">Career Path</span>
-            </a>
+          <li className='sidebar-nav-item'>
+            <Link href='/career-path' className='sidebar-link'>
+              <span className='sidebar-icon'>2</span>
+              <span className='sidebar-text'>Career Path</span>
+            </Link>
           </li>
-          <li className="sidebar-nav-item">
-            <a href="#" className="sidebar-link">
-              <span className="sidebar-icon">3</span>
-              <span className="sidebar-text">Hidden Player</span>
-            </a>
+          <li className='sidebar-nav-item'>
+            <Link href='/hidden-player' className='sidebar-link'>
+              <span className='sidebar-icon'>3</span>
+              <span className='sidebar-text'>Hidden Player</span>
+            </Link>
           </li>
-          <li className="sidebar-nav-item">
-            <a href="#" className="sidebar-link">
-              <span className="sidebar-icon">4</span>
-              <span className="sidebar-text">Higher or Lower</span>
-            </a>
+          <li className='sidebar-nav-item'>
+            <Link href='/higher-lower' className='sidebar-link'>
+              <span className='sidebar-icon'>4</span>
+              <span className='sidebar-text'>Higher or Lower</span>
+            </Link>
           </li>
         </ul>
       </div>
