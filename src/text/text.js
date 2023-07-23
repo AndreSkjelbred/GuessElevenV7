81792216a8ce227d216ecb285f90f7bf7bef828d
jdoyan042 <kirahunting2@gmail.com>
2023-07-24 00:48:13 +0200

    text

--- src/text/text.js
+++ src/text/text.js
@@ -4,6 +4,6 @@ export const guessElevenText =
 export const careerPathText =
   "Can you find the player based on their past clubs? This game requires knowledge about football history and transfer history. Good Luck!";
 
-export const blurredPlayerText = "igger";
+export const blurredPlayerText = "Welcome to Blurred Player : the ultimate football guessing game! Unveil blurred photos of football stars and guess their names. Test your knowledge now! Good luck!";
 
-export const higherLowerText = "igger";
+export const higherLowerText = "Welcome to Higher or Lower : the thrilling football market value guessing game! Think you know the worth of football stars? Put your skills to the test!";
