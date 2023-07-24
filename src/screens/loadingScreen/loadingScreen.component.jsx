c2dfba0926e83133ce8c4e17ad3b83c6943d1661
Isak Sagdalen <isakhsag@outlook.com>
2023-07-24 04:39:28 +0200

    game nav

--- src/screens/loadingScreen/loadingScreen.component.jsx
+++ src/screens/loadingScreen/loadingScreen.component.jsx
@@ -1,20 +1,17 @@
-import Image from "next/image";
-import logo from "./logo7.png";
-
 function LoadingScreen({ text }) {
   return (
-    <div className='loading-screen'>
-      <div className='loading-screen-text'>
-        <div className='section-loading'>
-          <div className='container-loading'>
-            <div className='content-section-loading'>
-              <div className='title-loading'>
+    <div className="loading-screen">
+      <div className="loading-screen-text">
+        <div className="section-loading">
+          <div className="container-loading">
+            <div className="content-section-loading">
+              <div className="title-loading">
                 <h1>Guess Eleven</h1>
               </div>
-              <div className='content-loading'>
+              <div className="content-loading">
                 <h3>Our Brand New Game</h3>
                 <p>{text}</p>
-                <div className='button-loading'>
+                <div className="button-loading">
                   <p>Enjoy!</p>
                 </div>
               </div>
@@ -23,7 +20,7 @@ function LoadingScreen({ text }) {
         </div>
       </div>
 
-      <div id='loader'></div>
+      <div id="loader"></div>
     </div>
   );
 }
